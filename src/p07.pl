a0d2aef84134bb54d5562f54476d791b8ac0799a
Darren Grant <dedgrant@gmail.com>
2020-12-06 22:13:35 -0800

    Unify rules whose inner bags directly reference a style.

--- src/p07.pl
+++ src/p07.pl
@@ -24,3 +24,16 @@ rules([R|Rules]) --> rule(R), rules(Rules).
 db(R) :- phrase_from_file(rules(R), "p07.txt"), !.
 
 % parsing complete
+
+% Inner bag Style.
+innerbag_style(_-Style, Style).
+
+% True when Style is a member of the given rule's InnerBags.
+member_inner_style(Style, _:InnerBags) :-
+    maplist(innerbag_style, InnerBags, InnerStyles),
+    member(Style, InnerStyles).
+
+% All Rules whose inner bags directly refer to the given Style.
+inner_style_rules(Style, Rules) :-
+    db(AllRules),
+    include(member_inner_style(Style), AllRules, Rules).
