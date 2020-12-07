373e481350ea2bcaa80023b792d1bfb9c23872b4
Darren Grant <dedgrant@gmail.com>
2020-12-06 22:39:42 -0800

    Added missing _no other bags_ parse (oops); solved part 1, but slowwwly.

--- src/p07.pl
+++ src/p07.pl
@@ -5,6 +5,7 @@ inner_bag(Count-Style) -->
     blanks, string(Style0), { atom_codes(Style, Style0) },
     blanks, ("bag" ; "bags").
 
+inner_bags([]) --> blanks, "no other bags.", blanks_to_nl.
 inner_bags([]) --> blanks_to_nl.
 inner_bags([B|Bags]) -->
     blanks, inner_bag(B), ("," ; "."), inner_bags(Bags).
@@ -34,6 +35,28 @@ member_inner_style(Style, _:InnerBags) :-
     member(Style, InnerStyles).
 
 % All Rules whose inner bags directly refer to the given Style.
-inner_style_rules(Style, Rules) :-
+inner_style_rules(InnerStyle, Rules) :-
     db(AllRules),
-    include(member_inner_style(Style), AllRules, Rules).
+    include(member_inner_style(InnerStyle), AllRules, Rules).
+
+% Outer bag style.
+rule_outer_style(OuterStyle:_, OuterStyle).
+
+% Fan out to discover all 
+% It's really slow :)
+inner_style_rules_fanout(InnerStyle, Rules) :-
+    inner_style_rules(InnerStyle, R0),
+    length(R0, N),
+    N >= 0,
+    maplist(rule_outer_style, R0, S0),
+    maplist(inner_style_rules_fanout, S0, ParentRules),
+    flatten([R0|ParentRules], RulesDups),
+    list_to_ord_set(RulesDups, Rules),
+    !.
+inner_style_rules_fanout(_, []).
+
+
+sol1(N) :-
+    inner_style_rules_fanout('shiny gold', Rules),
+    maplist(rule_outer_style, Rules, Styles),
+    length(Styles, N).
