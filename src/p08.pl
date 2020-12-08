d5a3c9c19ec5c33c6f2981b0a0e497108da9b6b7
Darren Grant <dedgrant@gmail.com>
2020-12-07 21:37:40 -0800

    Perform trace for sol1/1, but it is wrong :(

--- src/p08.pl
+++ src/p08.pl
@@ -27,3 +27,18 @@ pc_acc_step(Pc0, Acc0, Pc, Acc) :-
     prog(I),
     nth0(Pc0, I, Op:Val),
     pc_acc_op_val_step(Pc0, Acc0, Op, Val, Pc, Acc).
+
+% Trace the program path until we revisit a pc value in our history.
+% The state of the accumulator just before revisiting a pc for the
+% first time is LastAcc.
+trace_(Pc0, _, History, LastAcc) :- member(Pc0:LastAcc, History).
+trace_(Pc0, Acc0, History, LastAcc) :-
+    \+ member(Pc0:_, History),
+    pc_acc_step(Pc0, Acc0, Pc1, Acc1),
+    trace_(Pc1, Acc1, [Pc0:Acc0|History], LastAcc).
+
+trace(LastAcc) :- trace_(0, 0, [], LastAcc).
+
+sol1(N) :- trace(N).
+% sol1(N), N = 118 is the solution I get but it is wrong.
+
